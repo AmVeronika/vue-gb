b7095118b479406068d7a0d8c9b3e184ccb0f384
AM <mail@mail.ru>
2021-09-27 17:03:57 +0300

    lesson4

---
+++ vue_hw-4/src/components/pages.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="btn-pages">
+    <button
+      class="btn-page"
+      @click="paginatedData(ind)"
+      v-for="(i, ind) in pageCount"
+      :key="ind"
+    >
+      {{ i }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pages",
+  props: {
+    items: Array,
+    sizeItem: Number,
+  },
+  methods: {
+    paginatedData(key) {
+      return this.$emit("paginatedData", key);
+      // this.paymentsList.slice(start, end);
+    },
+  },
+  computed: {
+    pageCount() {
+      let l = this.items.length,
+        s = this.sizeItem;
+      return Math.ceil(l / s);
+    },
+  },
+};
+</script>
+<style scoped>
+.btn-pages {
+  width: fit-content;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+}
+.btn-page {
+  padding: 10px;
+}
+</style>
---
+++ vue_hw-4/src/components/paymentsList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="payments-list">
+    <ul class="payments-ul">
+      <ul class="payments-list-title">
+        <li class="item item-title">#</li>
+        <li class="item item-title">Date</li>
+        <li class="item item-title">Category</li>
+        <li class="item item-title">Value</li>
+      </ul>
+      <ul class="payments-list-punkt" v-for="(i, ind) in items" :key="ind">
+        <li class="item item-value">{{ ind +1 }}</li>
+        <li class="item item-value">{{ i.date }}</li>
+        <li class="item item-value">{{ i.category }}</li>
+        <li class="item item-value">{{ i.price }} rub</li>
+      </ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "paymentsList",
+  props: {
+    items: Array,
+  },
+};
+</script>
+
+<style scoped>
+.payments-ul {
+  padding-left: 0px;
+  height: 190px;
+}
+.payments-list-title,
+.payments-list-punkt {
+  display: flex;
+  list-style-type: none;
+  padding-left: 0;
+  row-gap: 20px;
+  justify-content: space-between;
+}
+.item {
+  padding: 5px;
+}
+.item-title,
+.item-value {
+  width: 100%;
+  font-weight: 700;
+  border: 1px solid black;
+}
+.item-value {
+  font-weight: 500;
+}
+</style>
