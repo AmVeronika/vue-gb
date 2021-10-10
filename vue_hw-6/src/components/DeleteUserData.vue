5d722f73769ee0b4ce7f14be2f623fda0c5dbf7a
Veronika Mikhotina <mihotinaw@gmail.com>
2021-10-10 22:24:12 +0300

    lesson6 var3

---
+++ vue_hw-6/src/components/DeleteUserData.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="delete-data">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeleteUserData",
+};
+</script>
+<style scoped>
+</style>
---
+++ vue_hw-6/src/components/EditUserData.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="edit-user-data">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditUserData",
+};
+</script>
+<style scoped>
+</style>
